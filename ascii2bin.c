8da1e8581ef2c7d7ca863bc08884a03dd2e40e26
SealAWrap <jeovanyguillen.j@gmail.com>
2021-03-04 12:16:57 -0800

    Added the final validation of if it overpass the boundary

--- ascii2bin.c
+++ ascii2bin.c
@@ -4,22 +4,29 @@
 
 int main (int argc, char * argv[], char ** envp) {
 
-    char offset = 0x30;  //0x30 is the octal base of decmial 48
+    char offset = 0x30;  //0x30 is the hexadecimal base of decmial 48
     int number = 0;
-    int retval;
+    int retval, count = 0;
     char ascii_value, digit;
         
     retval = read(0, &ascii_value, 1);
     while (retval == 1) {
+        digit = ascii_value - offset;
+        //validation if its a 0 or a 1
         if (ascii_value == offset || ascii_value == 0x31) {
-            digit = ascii_value - offset;
             number = (number << 1) + digit;  
             retval = read(0, &ascii_value, 1);
+            count++;
         } else {
             retval = 0;
         }
     }
 
-    printf("%u\n", number);
-    return 0;
-}
+    //validation whether , its a 1, 0, newline, neither, or the bit size is over bound
+    if ((ascii_value == 0x30 || ascii_value == 0x31 || ascii_value == 0x0a) && MAX_POWER >= count) { //0x0a is the newline char in hexadecial
+        printf("%u\n", number);
+        return 0;
+    } else {
+        return 1; //when its neither
+    }
+}//end of main method
