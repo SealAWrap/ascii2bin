44ef92f2bbb73ba0c89b370f03cd2ffe39d2f379
SealAWrap <jeovanyguillen.j@gmail.com>
2021-03-04 11:11:00 -0800

    Added the main method and variable types to the algorithmn

--- ascii2bin.c
+++ ascii2bin.c
@@ -1,11 +1,26 @@
-offset = 48;
-number = 0;
-    
-retval = read(0, &ascii_value, 1);
-while (retval == 1)
-    digit = ascii_value - offset;
-    number = (number << 1) + digit;  
-    retval = read(0, &ascii_value, 1);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main (int argc, char * argv[], char ** envp) {
+
+    int offset = 48;
+    int number = 0;
+    int retval;
+    char ascii_value, digit;
+    int number;
         
-printf("%u\n", number);
-return 0;
+    retval = read(0, &ascii_value, 1);
+    while (retval == 1) {
+        if (ascii_value == offset || ascii_value == offset + 1) {
+            digit = ascii_value - offset;
+            number = (number << 1) + digit;  
+            retval = read(0, &ascii_value, 1);
+        } else {
+            retval = 0;
+        }
+    }
+    
+    printf("%u\n", number);
+    return 0;
+}
